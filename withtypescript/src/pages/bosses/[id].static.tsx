dc7323e1ffe84728a3192a117a6b8ef5217d94e3
andreasbaade <andreasbaade@hotmail.com>
2023-02-17 09:27:17 +0100

    Static Routes, and serve routes

--- withtypescript/src/pages/bosses/[id].static.tsx
+++ withtypescript/src/pages/bosses/[id].static.tsx
@@ -1,13 +1,11 @@
+
+
 import Image from "next/image";
 import imageLoader from "../../../imageLoader";
-import {Datum} from "../../../types";
-import {useRouter} from "next/router";
-import {GetServerSideProps} from "next";
+import {Bosses, Datum} from "../../../types";
 
 function BossPage({ boss }: { boss: Datum }) {
-    const router = useRouter()
     console.log("Promise:", boss.data.name)
-    console.log(router.query)
     return (
         <div key={boss.data.id}>
             <h1>{boss.data.name}</h1>
@@ -24,9 +22,21 @@ function BossPage({ boss }: { boss: Datum }) {
     );
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export async function getStaticPaths() {
+    const res = await fetch("https://eldenring.fanapis.com/api/bosses");
+    const { data }: Bosses = await res.json();
+
+    return {
+        paths: data.map((boss) => {
+            return { params: { id: String(boss.id) } };
+        }),
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params }: { params: { id: string } }) {
     const res = await fetch(
-        `https://eldenring.fanapis.com/api/bosses/${context.query.id}`
+        `https://eldenring.fanapis.com/api/bosses/${params.id}`
     );
     const boss = await res.json();
 
